a87236bcd22f1ffa81a5dc57960a3c98d43bee62
Jack-dv7 <jack-lcf@yandex.ru>
2015-09-07 19:43:51 +0300

    2015.09.07

--- src/dao/mysql/Messages.java
+++ src/dao/mysql/Messages.java
@@ -7,4 +7,5 @@ public class Messages {
     public final static String CONNECTION_POOL_CREATE_ERROR = "Connection pool create error: ";
     public final static String CREATE_DAO_ERROR = "Create Dao instance error: ";
     public final static String CREATE_DAO_FACTORY_ERROR = "Create DaoFactory error: ";
+    public final static String ROLLBACK_ERROR = "Connection rollback error: ";
 }
